c7a9a5115d8665bae0b9d72012e392e9d02ee4d2
tuanbui0509 <tuanbui0509@gmail.com>
2021-07-12 22:19:05 +0700

    add swaager

--- TastyFoodSolution.BackendApi/Startup.cs
+++ TastyFoodSolution.BackendApi/Startup.cs
@@ -32,12 +32,15 @@ namespace TastyFoodSolution.BackendApi
 
             // Declare DI
             services.AddTransient<IPublicProductService, PublicProductService>();
+            services.AddSwaggerGen();
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -51,9 +54,19 @@ namespace TastyFoodSolution.BackendApi
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+
             app.UseRouting();
 
             app.UseAuthorization();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API TastyFoodSolution");
+            });
+
 
             app.UseEndpoints(endpoints =>
             {
